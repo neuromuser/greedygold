90516bb3ba93016ee6448f66a53b57bd61a1b4c5
neuromuser <neuromuser@gmail.com>
2026-01-21 20:27:11 +0200

    Critical bugfix (if is not golden tool - return)

--- src/main/java/com/neuromuser/greedygold/upgrade/ItemUpgradeManager.java
+++ src/main/java/com/neuromuser/greedygold/upgrade/ItemUpgradeManager.java
@@ -86,6 +86,7 @@ public class ItemUpgradeManager {
     public static void onItemUsed(ItemStack stack, ServerPlayerEntity player) {
         ConfigValues config = ModConfig.getInstance().getValues();
         if (!config.upgradesEnabled) return;
+        if (!isGoldenItem(stack)) return;
 
         ItemUpgradeData data = getData(stack);
         double affinity = getAffinity(stack);
@@ -236,7 +237,19 @@ public class ItemUpgradeManager {
 
         return Math.max(0, requiredUses - data.getEnchantUses());
     }
+    private static boolean isGoldenItem(ItemStack stack) {
+        Item item = stack.getItem();
 
+        if (item instanceof ToolItem toolItem) {
+            return toolItem.getMaterial() == ToolMaterials.GOLD;
+        }
+
+        if (item instanceof ArmorItem armorItem) {
+            return armorItem.getMaterial() == ArmorMaterials.GOLD;
+        }
+
+        return false;
+    }
 
 }
 
